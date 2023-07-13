549be6b909fc3a509cf3e0e7ca3c5025be7c1b0a
komark06 <komark06@gmail.com>
2023-07-13 12:23:19 +0800

    Change port to 8080

--- entry.sh
+++ entry.sh
@@ -5,4 +5,4 @@ CORES=$(nproc)
 WORKERS=$((CORES * 2 + 1))
 
 # Start Gunicorn with the specified number of workers
-exec gunicorn -b 0.0.0.0:8000 --backlog 1 --workers $WORKERS --threads 1 --graceful-timeout 0 --timeout 0 app:app
+exec gunicorn -b 0.0.0.0:8080 --backlog 1 --workers $WORKERS --threads 1 --graceful-timeout 0 --timeout 0 app:app
